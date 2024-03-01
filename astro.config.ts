42a359e5c8519b9db33fb50c455b76624076c4ff
Licodeao <1019805199@qq.com>
2024-03-01 11:28:40 +0800

    feat: robots and sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import rehypePrettyCode from "rehype-pretty-code";
+import sitemap from "@astrojs/sitemap";
 
 const options = {
   theme: "one-dark-pro",
@@ -7,6 +8,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [[rehypePrettyCode, options]],
